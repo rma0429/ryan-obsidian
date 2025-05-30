ec1290f899de345d1326289ba44699bde3f4099e
rma0429 <rma0429@gmail.com>
2025-05-30 11:45:00 +0800

    Update Quartz site with correct base URL and audio assets

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ryan.Git",
+    pageTitle: "Ryan-Obsidian",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "Ryan.Git",
+    baseUrl: "ryan-obsidian",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
